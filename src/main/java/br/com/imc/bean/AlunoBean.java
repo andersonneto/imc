c10a3bb719fc6977d0374152316840028f524d64
Anderson Marques Neto <andersonneto@msn.com>
2024-04-10 22:29:52 -0300

    Melhorias no cadastro do Aluno

---
+++ src/main/java/br/com/imc/bean/AlunoBean.java
@@ -0,0 +1,62 @@
+package br.com.imc.bean;
+
+import br.com.imc.DAO.TurmaDAO;
+import br.com.imc.domain.Turma;
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import org.omnifaces.cdi.Param;
+import org.omnifaces.util.Messages;
+
+@Named
+@ViewScoped
+public class TurmaBean implements Serializable {
+
+    @Param
+    private Integer id;
+
+    private Turma turma;
+    private TurmaDAO turmaDAO;
+
+    @PostConstruct
+    public void init() {
+        turma = new Turma();
+        turmaDAO = new TurmaDAO();
+
+        if (id != null) {
+            turma = turmaDAO.buscar(id);
+        }
+    }
+
+    public void novo() {
+    }
+
+    public void salvar() {
+        try {
+            System.out.println(turma.toString());
+            TurmaDAO turmaDAO = new TurmaDAO();
+            turma = turmaDAO.merge(turma);
+
+            Messages.addGlobalInfo("Dados salvos com sucesso");
+        } catch (RuntimeException e) {
+            Messages.addGlobalError("Erro ao tentar salvar os dados");
+        }
+    }
+
+    public Turma getTurma() {
+        return turma;
+    }
+
+    public void setTurma(Turma turma) {
+        this.turma = turma;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/br/com/imc/bean/AlunosBean.java
@@ -0,0 +1,52 @@
+package br.com.imc.bean;
+
+import br.com.imc.DAO.TurmaDAO;
+import br.com.imc.domain.Turma;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import org.omnifaces.util.Messages;
+
+@Named
+@ViewScoped
+public class TurmasBean implements Serializable {
+
+    private List<Turma> turmas;
+    private TurmaDAO turmaDAO;
+
+    /**
+     * Método a ser chamado no inicio do carregamento da página
+     */
+    @PostConstruct
+    public void init() {
+        try {
+            turmaDAO = new TurmaDAO();
+            turmas = turmaDAO.listar();
+        } catch (RuntimeException e) {
+            Messages.addGlobalError("Erro ao tentar listar os dados");
+        }
+    }
+
+    public void novo() {
+    }
+
+    public void excluir(Turma turma) {
+        try {
+            turmaDAO.excluir(turma);
+            turmas = turmaDAO.listar();
+            Messages.addGlobalInfo("Turma excluída com sucesso");
+        } catch (RuntimeException e) {
+            Messages.addGlobalError("Erro ao tentar excluir a turma");
+        }
+    }
+
+    public List<Turma> getTurmas() {
+        return turmas;
+    }
+
+    public void setTurmas(List<Turma> turmas) {
+        this.turmas = turmas;
+    }
+}
